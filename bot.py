2c9188cbec0de4884d06497d93520effb3bd8ba3
Alan Lozano <256232189+alanfabzlab@users.noreply.github.com>
2026-09-15 20:50:43 -0600

    feat: add  command and requests dependency

--- bot.py
+++ bot.py
@@ -4,9 +4,7 @@ import requests
 import json
 from dotenv import load_dotenv
 
-# Carga el token desde el archivo .env
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
 
 def get_meme():
     response = requests.get('https://meme-api.com/gimme')
@@ -15,22 +13,20 @@ def get_meme():
 
 class MyClient(discord.Client):
     async def on_ready(self):
-        print(f'Bot conectado con éxito como: {self.user}')
+        print(f'Logged on as {self.user}!')
 
     async def on_message(self, message):
         if message.author == self.user:
             return
 
         if message.content.startswith('$hello'):
-            await message.channel.send('Hello World!')
+            await message.channel.send('Hello!')
 
         if message.content.startswith('$meme'):
-            async with message.channel.typing():
-                meme_url = get_meme()
-                await message.channel.send(meme_url)
+            await message.channel.send(get_meme())
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = MyClient(intents=intents)
-client.run(TOKEN)
+client.run(os.getenv('DISCORD_TOKEN'))
